abee2017756d7e2780cc91de5f747a5a3bc5f14c
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-01-29 13:00:14 +0100

    Update retrospective.py

--- Azurance/app_views/raininsurance/retrospective.py
+++ Azurance/app_views/raininsurance/retrospective.py
@@ -83,7 +83,7 @@ def retrospective(request):
                                                                         form['dailyMaxTurnover'].value(),
                                                                         form['fixedCosts'].value())
 
-            x_coordinates = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+            x_coordinates = ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022']
             y_coordinates = []
             for annee in x_coordinates:
                 premiuma, covereda, notcovereda, ca, nca, cma, ncma = computeRetro(form['location'].value(),
