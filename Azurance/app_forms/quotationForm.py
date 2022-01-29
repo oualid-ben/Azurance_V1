2b651ecfc7d34ece9323ff5143032920e21ef7a5
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2022-01-29 15:57:25 +0100

    chart

--- Azurance/app_forms/quotationForm.py
+++ Azurance/app_forms/quotationForm.py
@@ -16,16 +16,16 @@ cities = (
 
 class QuotationForm(forms.Form):
     clientName = forms.CharField(label="Nom de l'entreprise" ,max_length=100)
-    clientName.widget.attrs.update({'class': 'form-control'})
+    clientName.widget.attrs.update({'class': 'form-control', 'value':'Amazon'})
 
     dailyMaxTurnover = forms.FloatField(label="Chiffre d'affaire journalier maximum (€)")
-    dailyMaxTurnover.widget.attrs.update({'class': 'form-control'})
+    dailyMaxTurnover.widget.attrs.update({'class': 'form-control','value':1200})
 
     fixedCosts = forms.FloatField(label="Coûts fixes journalier (€)")
-    fixedCosts.widget.attrs.update({'class': 'form-control'})
+    fixedCosts.widget.attrs.update({'class': 'form-control','value':400})
 
     rainfall = forms.FloatField(label="Niveau de pluie journalier pivot (mm)")
-    rainfall.widget.attrs.update({'class': 'form-control'})
+    rainfall.widget.attrs.update({'class': 'form-control','value':2})
 
     subscriptionDate = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class':'form-control','pattern':'\\d{4}-\\d{2}-\\d{2}' , 'value': DtT.datetime.today().strftime('%Y-%m-%d')}), label='Subscription Date')
 
--- Azurance/app_forms/retroForm.py
+++ Azurance/app_forms/retroForm.py
@@ -17,19 +17,19 @@ cities = (
 
 class RetroForm(forms.Form):
     clientName = forms.CharField(label="Nom de l'entreprise", max_length=100)
-    clientName.widget.attrs.update({'class': 'form-control'})
+    clientName.widget.attrs.update({'class': 'form-control', 'value':'Amazon'})
 
     dailyMaxTurnover = forms.FloatField(label="Chiffre d'affaire journalier maximum (€)")
-    dailyMaxTurnover.widget.attrs.update({'class': 'form-control'})
+    dailyMaxTurnover.widget.attrs.update({'class': 'form-control','value':1200})
 
     fixedCosts = forms.FloatField(label="Coûts fixes journalier (€)")
-    fixedCosts.widget.attrs.update({'class': 'form-control'})
+    fixedCosts.widget.attrs.update({'class': 'form-control','value':400})
 
     rainfall = forms.FloatField(label="Niveau de pluie journalier pivot. (mm)")
-    rainfall.widget.attrs.update({'class': 'form-control'})
+    rainfall.widget.attrs.update({'class': 'form-control','value':2})
 
     subscriptionDate = forms.CharField(label="Année rétrospective", max_length=4)
-    subscriptionDate.widget.attrs.update({'class': 'form-control'})
+    subscriptionDate.widget.attrs.update({'class': 'form-control', 'value':'2020'})
 
     location = forms.ChoiceField(label="Ville", choices=cities)
     location.widget.attrs.update({'class': 'form-control'})
